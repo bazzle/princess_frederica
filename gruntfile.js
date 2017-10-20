4bb0667c5e0f8f8a483afb21f263d4c6f4999da1
Barry Richards <barry@br-web.io>
2017-10-20 15:29:56 +0100

    CSS structure setup

--- gruntfile.js
+++ gruntfile.js
@@ -1,16 +1,48 @@
 module.exports = function(grunt) {
-    
-   grunt.initConfig({
-     sass: {
-       dist: {
-         files: {
-           'main.css': 'main.scss'
-         }
-       }
-     }
-   });
-    
-     // Load grunt plugins.
-   grunt.loadNpmTasks('grunt-contrib-sass');
-      
-   };
+  
+    grunt.initConfig({
+      postcss: {
+        options: {
+          map: true, // inline sourcemaps
+  
+          processors: [
+            require('pixrem')(), // add fallbacks for rem units 
+            require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes 
+            require('cssnano')() // minify the result 
+          ]
+        },
+        dist: {
+          files: {
+            'css/main.css': 'css/main.scss'
+          }
+        }
+      },
+      sass: {
+        dist: {
+          files: {
+                'css/main.css': 'sass/main.scss'
+              }
+        }
+      },
+      watch: {
+        src: {
+            files: ['sass/*.scss'],
+            tasks: ['sass']
+        }
+      },
+      purifycss: {
+        options: {},
+        target: {
+          src: ['test/*.html'],
+          css: ['test/css/main.css'],
+          dest: 'test/css/purestyles.css'
+        }
+      }
+    });
+  
+  // Load grunt plugins.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-purifycss');
+  };
